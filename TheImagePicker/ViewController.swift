c24427fddcfc923a94a33e40771822880ecd6662
vdthatte <vd_thatte@yahoo.com>
2017-11-25 16:12:38 -0500

    initial commit

---
+++ TheImagePicker/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  TheImagePicker
+//
+//  Created by Vidyadhar V. Thatte on 11/25/17.
+//  Copyright © 2017 VdThatte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var uploadButton: UIButton!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.imagePicker.delegate = self
+    }
+
+    @IBAction func uploadTapped(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            self.imageView.contentMode = .scaleAspectFit
+            self.imageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
